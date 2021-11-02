8f60ddfc35daff303b88976ab576e785335dc94b
Rupesh <rupeshsharma2196@gmail.com>
2021-11-02 21:15:48 +0530

    added all the apis for all the component

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,87 +1,244 @@
 const express = require("express");
 const router = express.Router();
-// const gravatar = require("gravatar");
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
-// const config = require("config");
-// const { check, validationResult } = require("express-validator");
-// const normalize = require("normalize-url");
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
 
-// const User = require("../../models/User");
+const Post = require("../../models/Post");
+const User = require("../../models/User");
+const checkObjectId = require("../../middleware/checkObjectId");
 
-// @route    POST api/users
-// @desc     Register user
-// @access   Public
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
+router.post(
+	"/",
+	auth,
+	check("text", "Text is required").notEmpty(),
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-router.get("/", (req, res) => res.send("posts route"));
+		try {
+			const user = await User.findById(req.user.id).select("-password");
 
-// router.post(
-// 	"/",
-// 	check("name", "Name is required").notEmpty(),
-// 	check("email", "Please include a valid email").isEmail(),
-// 	check(
-// 		"password",
-// 		"Please enter a password with 6 or more characters"
-// 	).isLength({ min: 6 }),
-// 	async (req, res) => {
-// 		const errors = validationResult(req);
-// 		if (!errors.isEmpty()) {
-// 			return res.status(400).json({ errors: errors.array() });
-// 		}
+			const newPost = new Post({
+				text: req.body.text,
+				name: user.name,
+				avatar: user.avatar,
+				user: req.user.id,
+			});
 
-// 		const { name, email, password } = req.body;
+			const post = await newPost.save();
 
-// 		try {
-// 			let user = await User.findOne({ email });
+			res.json(post);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server Error");
+		}
+	}
+);
 
-// 			if (user) {
-// 				return res
-// 					.status(400)
-// 					.json({ errors: [{ msg: "User already exists" }] });
-// 			}
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Private
+router.get("/", auth, async (req, res) => {
+	try {
+		const posts = await Post.find().sort({ date: -1 });
+		res.json(posts);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
+});
 
-// 			const avatar = normalize(
-// 				gravatar.url(email, {
-// 					s: "200",
-// 					r: "pg",
-// 					d: "mm",
-// 				}),
-// 				{ forceHttps: true }
-// 			);
+// @route    GET api/posts/:id
+// @desc     Get post by ID
+// @access   Private
+router.get("/:id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
 
-// 			user = new User({
-// 				name,
-// 				email,
-// 				avatar,
-// 				password,
-// 			});
+		if (!post) {
+			return res.status(404).json({ msg: "Post not found" });
+		}
 
-// 			const salt = await bcrypt.genSalt(10);
+		res.json(post);
+	} catch (err) {
+		console.error(err.message);
 
-// 			user.password = await bcrypt.hash(password, salt);
+		res.status(500).send("Server Error");
+	}
+});
 
-// 			await user.save();
+// @route    DELETE api/posts/:id
+// @desc     Delete a post
+// @access   Private
+router.delete("/:id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
 
-// 			const payload = {
-// 				user: {
-// 					id: user.id,
-// 				},
-// 			};
+		if (!post) {
+			return res.status(404).json({ msg: "Post not found" });
+		}
 
-// 			jwt.sign(
-// 				payload,
-// 				config.get("jwtSecret"),
-// 				{ expiresIn: "5 days" },
-// 				(err, token) => {
-// 					if (err) throw err;
-// 					res.json({ token });
-// 				}
-// 			);
-// 		} catch (err) {
-// 			console.error(err.message);
-// 			res.status(500).send("Server error");
-// 		}
-// 	}
-// );
+		// Check user
+		if (post.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: "User not authorized" });
+		}
+
+		await post.remove();
+
+		res.json({ msg: "Post removed" });
+	} catch (err) {
+		console.error(err.message);
+
+		res.status(500).send("Server Error");
+	}
+});
+
+// @route    PUT api/posts/like/:id
+// @desc     Like a post
+// @access   Private
+router.put("/like/:id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// Check if the post has already been liked
+		if (
+			post.likes.filter((like) => like.user.toString() === req.user.id).length >
+			0
+		) {
+			return res.status(400).json({ msg: "Post already liked" });
+		}
+		// if (post.likes.some((like) => like.user.toString() === req.user.id)) {
+		// 	return res.status(400).json({ msg: "Post already liked" });
+		// }
+
+		post.likes.unshift({ user: req.user.id });
+
+		await post.save();
+
+		return res.json(post.likes);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
+});
+
+// @route    PUT api/posts/unlike/:id
+// @desc     Unlike a post
+// @access   Private
+router.put("/unlike/:id", auth, checkObjectId("id"), async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// Check if the post has not yet been liked
+		if (
+			post.likes.filter((like) => like.user.toString() === req.user.id)
+				.length === 0
+		) {
+			return res.status(400).json({ msg: "Post has not been liked" });
+		}
+
+		// if (!post.likes.some((like) => like.user.toString() === req.user.id)) {
+		// 	return res.status(400).json({ msg: "Post has not yet been liked" });
+		// }
+
+		// remove the like
+		const removeIndex = post.likes
+			.map((like) => like.user.toString())
+			.indexOf(req.user.id);
+
+		post.likes.splice(removeIndex, 1);
+
+		// post.likes = post.likes.filter(
+		// 	({ user }) => user.toString() !== req.user.id
+		// );
+
+		await post.save();
+
+		return res.json(post.likes);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
+});
+
+// @route    POST api/posts/comment/:id
+// @desc     Comment on a post
+// @access   Private
+router.post(
+	"/comment/:id",
+	auth,
+	check("text", "Text is required").notEmpty(),
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		try {
+			const user = await User.findById(req.user.id).select("-password");
+			const post = await Post.findById(req.params.id);
+
+			const newComment = {
+				text: req.body.text,
+				name: user.name,
+				avatar: user.avatar,
+				user: req.user.id,
+			};
+
+			post.comments.unshift(newComment);
+
+			await post.save();
+
+			res.json(post.comments);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server Error");
+		}
+	}
+);
+
+// @route    DELETE api/posts/comment/:id/:comment_id
+// @desc     Delete comment
+// @access   Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// Pull out comment
+		const comment = post.comments.find(
+			(comment) => comment.id === req.params.comment_id
+		);
+		// Make sure comment exists
+		if (!comment) {
+			return res.status(404).json({ msg: "Comment does not exist" });
+		}
+		// Check user
+		if (comment.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: "User not authorized" });
+		}
+
+		const removeIndex = post.comments
+			.map((comment) => comment.user.toString())
+			.indexOf(req.user.id);
+
+		post.comments.splice(removeIndex, 1);
+
+		// post.comments = post.comments.filter(
+		// 	({ id }) => id !== req.params.comment_id
+		// );
+
+		await post.save();
+
+		return res.json(post.comments);
+	} catch (err) {
+		console.error(err.message);
+		return res.status(500).send("Server Error");
+	}
+});
 
 module.exports = router;
