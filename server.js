8be5ae574f7ac25fca6f304c71136c1d1c22a532
Rupesh <rupeshsharma2196@gmail.com>
2021-11-04 17:00:15 +0530

    final modification

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
-//const path = require("path");
+const path = require("path");
 
 const app = express();
 
@@ -10,12 +10,20 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
-
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("frontend/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
